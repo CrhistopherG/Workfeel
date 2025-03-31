1c9072857866b0b3ab5add4a667789bd31fbaacd
Crhistopher Gutierrez <crhistopherisai@gmail.com>
2025-03-31 15:46:22 -0600

    qwerty

--- frontend/src/components/navbar-sidebar/NavBar.tsx
+++ frontend/src/components/navbar-sidebar/NavBar.tsx
@@ -1,3 +1,4 @@
+import { FaRegUserCircle } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoIosNotifications } from "react-icons/io";
 import { Link } from "react-router-dom";
@@ -5,20 +6,22 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <header className="bg-gray-300 shadow-black h-15 shadow flex justify-between">
+    <header className="bg-gray-300 shadow-black lg max-h-17 shadow flex justify-between">
         {/* */}
         <div className="mx-4 max-w-6xl py-2 flex  text-start  items-center">
             <button className="ms-2" aria-label="Toggle Sidebar">
                 <GiHamburgerMenu className="lg:w-8 lg:h-8 md:w-6 md:h-6 w-4 h-8"/>
             </button>
             <Link to="/periodo">
-                <img className=" lg:w-xs md:w-2xs w-50" src="/workfeel-logo.png" alt="Workfeel Logo"/>
+                <img className=" lg:w-xs md:w-2xs w-40" src="/workfeel-logo.png" alt="Workfeel Logo"/>
             </Link>
             
         </div>
         <div className="mx-4 max-w-6xl py-2 flex justify-between text-end  items-center">
-            <IoIosNotifications className="w-10 h-10 mx-"/>
-            <h1 className="text-xl text-black">
+            <IoIosNotifications className="lg:w-8 lg:h-8 md:w-6 md:h-6 w-4 h-8 mx-1"/>
+            <FaRegUserCircle className="lg:w-8 lg:h-8 md:w-6 md:h-6 w-4 h-8 mx-1" />
+
+            <h1 className="lg:text-lg md:text-sm text-xs text-black mx-1 text-nowrap">
                  Bienvenido Crhistopher Gutierrez   
             </h1>
         </div>
--- frontend/src/components/navbar-sidebar/SideBar.tsx
+++ frontend/src/components/navbar-sidebar/SideBar.tsx
@@ -1,9 +1,10 @@
-
+import { FaCalendarAlt, FaChartBar, FaCogs, FaPoll } from "react-icons/fa"
+import { Link } from "react-router-dom"
 
 export default function SideBar() {
   return (
-    <nav>
-        
-    </nav>
+    <>
+    
+    </>
   )
 }
