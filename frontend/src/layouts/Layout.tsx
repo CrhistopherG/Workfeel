765a523d515264003eaa1b9f3247fee4082ca2c8
Wilbert Oliver Chan Uc <131514400+wilbert2004@users.noreply.github.com>
2025-04-03 13:03:03 -0600

    avances de renderizacion
    avances de renderizacion del proyecto del cada componente

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -3,20 +3,24 @@ import NavBar from "../components/navbar-sidebar/NavBar"
 import SideBar from "../components/navbar-sidebar/SideBar"
 
 export default function Layout() {
+  const toggleSidebar = () => {
+    // Add sidebar toggle logic here
+  };
+
   return (
     <>
-        <NavBar />
-        <div className="flex flex-row">
-            <div>
-              <SideBar/>
-            </div>
-            <div className="justify-center w-full">
-              <main className="mt-10 mx-5 p-5 bg-white shadow-lg justify-items-center">
-                  <Outlet />
-              </main>
-            </div>
+      <NavBar toggleSidebar={toggleSidebar} />
+      <div className="flex flex-row">
+        <div>
+          <SideBar />
         </div>
-    
+        <div className="justify-center w-full">
+          <main className="mt-10 mx-5 p-5 bg-white shadow-lg justify-items-center">
+            <Outlet />
+          </main>
+        </div>
+      </div>
+
     </>
   )
 }
