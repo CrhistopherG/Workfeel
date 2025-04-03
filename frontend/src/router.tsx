365de2c0b42d81498c227738d78727148a695bfd
Crhistopher Gutierrez <crhistopherisai@gmail.com>
2025-04-03 11:29:00 -0600

    Update router.tsx

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -17,10 +17,6 @@ export const router = createBrowserRouter([
         element: <Login />
     },
     {
-        path:'/periodo',
-        element: <Periodo />
-    },
-    {
         path: '/encuestas',
         element: <Layout />,
         children: [
