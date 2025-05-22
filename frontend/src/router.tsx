1efbfb15c0176146e927305bd21eeddd331c1330
Wilbert Oliver Chan Uc <131514400+wilbert2004@users.noreply.github.com>
2025-05-22 02:33:26 -0600

    Update router.tsx

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -8,110 +8,25 @@ import Escalas from './views/Escalas'
 import Periodo, { loader as PeriodoLoader } from './components/Periodo/Periodo'
 import ProtectedRoute from './auth/ProtectedRoute'
 import ModuloPanelAdmin from './components/navbar-sidebar/Modulos/ModuloPanelAdmin'
-import ListaPuestos from './components/Generales/ListaPuestos'
-import PlanAccion from './components/Generales/PlanAccion'
-import Configuraciones from './components/Generales/Configuraciones'
-import Resultado from './components/Resultados/Resultado'
-import NivelesSatisfaccion from './components/Resultados/NivelesSatisfaccion'
-import CompararPeriodos from './components/Resultados/CompararPeriodos'
-import Register, {action as RegisterAction} from './views/Register'
-
-
-export const router = createBrowserRouter([
-  {
-    path: '/login',
-    element: <Login />
-  },
-  {
-    path: '/register',
-    element: <Register />,
-    action: RegisterAction
-  },
-  {
-    path: '/',
-    element: <ProtectedRoute />,
-    children: [
-      {
-
 import Dimensiones from './views/Dimensiones'
 import CompanyList, { loader as companyLoader } from './views/CompanyList'
 import NewCompany, { action as newCompanyAction } from './views/NewCompany'
-//importamos el action de agregar dimension
 import AgregarDimensiones from './components/Encuestas/AgregarDimensiones'
-import EditarDimensiones from './components/Encuestas/EditarDimensiones';
+import EditarDimensiones from './components/Encuestas/EditarDimensiones'
 import AgregarPreguntas from './components/Encuestas/AgregarPreguntas'
 import EditarPreguntas from './components/Encuestas/EditarPreguntas'
 import AgregarEscalas from './components/Encuestas/AgregarEscalas'
 import EditarScale from './components/Encuestas/EditarScale'
-//importamos el action de agregar dimension
+
 export const router = createBrowserRouter([
     {
         path: '/login',
         element: <Login />
     },
     {
-
         path: '/',
         element: <ProtectedRoute />,
         children: [
-
-          {
-            path: '/api/periodo',
-            element: <Periodo />,
-            loader: PeriodoLoader
-          },
-          {
-            path: '/encuestas/dimensiones',
-            element: <Dimensiones />,
-            loader: DimensionLoader
-          },
-          {
-            path: '/encuestas/preguntas',
-            element: <Preguntas />
-          },
-          {
-            path: '/encuestas/escalas',
-            element: <Escalas />
-          },
-          {
-            path: '/panel_administrativo',
-            element: <ModuloPanelAdmin />
-          },
-          {
-            path: '/panel_administrativo/usuarios/ver_todos',
-            element: <User />,
-            loader: usersLoader
-          },
-          {
-            path: '/panel_administrativo/usuarios/nuevo',
-            element: <NewUser />,
-            action: newUserAction
-          },
-          {
-            path: '/generales/lista_puestos',
-            element: <ListaPuestos />
-          },
-          {
-            path: '/generales/plan_accion',
-            element: <PlanAccion />
-          },
-          {
-            path: '/generales/configuraciones',
-            element: <Configuraciones />
-          },
-          {
-            path: '/resultados/resultado',
-            element: <Resultado />
-          },
-          {
-            path: '/resultados/niveles_satisfaccion',
-            element: <NivelesSatisfaccion />
-          },
-          {
-            path: '/resultados/comparar_periodos',
-            element: <CompararPeriodos />
-          },
-
             {
                 path: '/',
                 element: <Layout />,
@@ -123,18 +38,21 @@ export const router = createBrowserRouter([
                     },
                     {
                         path: '/encuestas/dimensiones',
-                        element: <Dimensiones />,
-                    }, {
+                        element: <Dimensiones />
+                    },
+                    {
                         path: '/encuestas/dimensiones/agregar',
                         element: <AgregarDimensiones />
-                    }, {
+                    },
+                    {
                         path: '/encuestas/dimensiones/:id/editar',
                         element: <EditarDimensiones />
                     },
                     {
                         path: '/encuestas/preguntas',
                         element: <Preguntas />
-                    }, {
+                    },
+                    {
                         path: '/encuestas/preguntas/agregar',
                         element: <AgregarPreguntas />
                     },
@@ -167,6 +85,16 @@ export const router = createBrowserRouter([
                         path: '/panel_administrativo/usuarios/nuevo',
                         element: <NewUser />,
                         action: newUserAction
+                    },
+                    {
+                        path: '/empresas/lista',
+                        element: <CompanyList />,
+                        loader: companyLoader
+                    },
+                    {
+                        path: '/empresas/nueva',
+                        element: <NewCompany />,
+                        action: newCompanyAction
                     }
                 ]
             }
